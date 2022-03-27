20e81e2c42037168cc1b3b52d6d1af5868bae876
_OfTeN_ <dkoften@gmail.com>
2022-03-27 13:45:34 +0300

    - Added SpecializedCrates converter (/ecocrates convert SpecializedCrates)

---
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecocrates/converters/impl/SpecializedCratesConverter.kt
@@ -0,0 +1,134 @@
+package com.willfp.ecocrates.converters.impl
+
+import com.willfp.eco.core.config.BuildableConfig
+import com.willfp.eco.core.config.interfaces.Config
+import com.willfp.ecocrates.EcoCratesPlugin
+import com.willfp.ecocrates.converters.Converter
+import com.willfp.ecocrates.converters.util.ConversionHelpers
+import com.willfp.ecocrates.converters.util.toLookupString
+import com.willfp.ecocrates.crate.Crates
+import com.willfp.ecocrates.crate.placed.PlacedCrates
+import me.PM2.customcrates.crates.Crate
+import me.PM2.customcrates.crates.PlacedCrate
+import me.PM2.customcrates.crates.options.rewards.Reward
+import org.bukkit.Location
+
+class SpecializedCratesConverter(private val plugin: EcoCratesPlugin,
+                                 override val id: String = "SpecializedCrates") : Converter {
+    override fun convert() {
+        val newCrates = Crate.getLoadedCrates().map { convertCrate(it.value) }
+
+        val crates = plugin.cratesYml.getSubsections("crates").toMutableList()
+
+        crates.addAll(newCrates)
+
+        plugin.cratesYml.set("crates", crates)
+        plugin.cratesYml.save()
+        plugin.rewardsYml.save()
+        plugin.reload()
+
+        val pCrates = PlacedCrate.getPlacedCrates().map { PCrate(it.key, Crates.getByID(it.value.crate.name.lowercase())!!) }
+
+        val toDelete = ArrayList(PlacedCrate.getPlacedCrates().values)
+
+        toDelete.forEach { it.delete() }
+
+        pCrates.forEach {
+            PlacedCrates.setAsCrate(it.location, it.crate)
+        }
+    }
+
+    private data class PCrate(val location: Location, val crate: com.willfp.ecocrates.crate.Crate)
+
+    private fun convertCrate(crate: Crate): Config {
+        val result = ConversionHelpers.createEmptyCrate()
+
+        val id = crate.name.lowercase()
+
+        result.set("id", id)
+        result.set("name", crate.name)
+        result.set("preview.title", crate.settings.crateInventoryName)
+        result.set("key.item", crate.settings.keyItemHandler.item.stack.toLookupString())
+        result.set("key.lore", crate.settings.keyItemHandler.item.lore)
+        result.set("keygui.item", "tripwire_hook unbreaking:1 hide_enchants name:\"${crate.name}\"")
+        result.set("keygui.lore", mutableListOf(
+            "<g:#56ab2f>${crate.name}</g:#a8e063>",
+            "&fYou have %keys% keys",
+            "&fGet more at &astore.example.net"
+        ))
+        result.set("keygui.shift-left-click-messsage", mutableListOf(
+            "Buy a ${crate.name}&r key here! &astore.example.net"
+        ))
+        result.set("placed.hologram.height", crate.settings.hologramOffset)
+        result.set("placed.hologram.frames", mutableListOf(
+            BuildableConfig()
+                .add("tick", 0)
+                .add("lines", crate.settings.hologram.lines)
+        ))
+        result.set("open.broadcasts", mutableListOf("%player%&f is opening the ${crate.name}!"))
+        result.set("finish.broadcasts", mutableListOf("%player%&f won %reward%&f from the ${crate.name}!"))
+
+        if (crate.settings.reward != null) {
+            val newRewards = mutableListOf<Config>()
+            var row = 2
+            var col = 2
+            var counter = 1
+            crate.settings.reward.crateRewards.forEach {
+                val salt = id + "_" + counter
+                newRewards.add(convertReward(it, salt, row, col))
+                col++
+                if (col >= 8) {
+                    col = 2
+                    row++
+                }
+                if (row >= 5) {
+                    row = 2
+                }
+                counter++
+            }
+
+            val rewards = plugin.rewardsYml.getSubsections("rewards").toMutableList()
+
+            rewards.addAll(newRewards)
+
+            plugin.rewardsYml.set("rewards", rewards)
+            result.set("rewards", newRewards.map { it.getString("id") })
+        }
+
+        return result
+    }
+
+    private fun convertReward(reward: Reward, salt: String, row: Int, col: Int): Config {
+        val result = ConversionHelpers.createEmptyReward()
+
+        result.set("id", salt)
+
+        result.set("commands", reward.commands
+            .map { it.replace(
+                "{name}", "%player%"
+            ) }
+        )
+        if (reward.isGiveDisplayItem) {
+            val item = reward.displayBuilder.stack
+            val meta = item.itemMeta!!
+            meta.lore = if (reward.isGiveDisplayItemLore) {
+                reward.displayBuilder.lore
+            } else {
+                mutableListOf()
+            }
+            meta.setDisplayName(reward.displayBuilder.getDisplayName(true))
+            result.set("items", mutableListOf(item.toLookupString()))
+        }
+
+        result.set("weight.actual", reward.chance)
+        result.set("weight.display", reward.chance)
+
+        result.set("display.name", reward.displayBuilder.getDisplayName(false))
+        result.set("display.item", reward.displayBuilder.stack.toLookupString())
+        result.set("display.lore", reward.displayBuilder.lore)
+        result.set("display.row", row)
+        result.set("display.column", col)
+
+        return result
+    }
+}
