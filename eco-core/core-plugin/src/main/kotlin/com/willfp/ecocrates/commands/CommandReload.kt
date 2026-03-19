c051c307122d5707dc51c290dae2d9ad70dd9662
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-03-19 13:40:39 +0000

    Update reload command message to include loaded crates, rewards, and keys

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecocrates/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecocrates/commands/CommandReload.kt
@@ -3,7 +3,11 @@ package com.willfp.ecocrates.commands
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
+import com.willfp.ecocrates.crate.Crate
+import com.willfp.ecocrates.crate.Crates
+import com.willfp.ecocrates.crate.Keys
 import com.willfp.ecocrates.plugin
+import com.willfp.ecocrates.reward.Rewards
 import org.bukkit.command.CommandSender
 
 object CommandReload : Subcommand(
@@ -16,6 +20,9 @@ object CommandReload : Subcommand(
         sender.sendMessage(
             plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
                 .replace("%time%", plugin.reloadWithTime().toNiceString())
+                .replace("%crates%", Crates.values().size.toString())
+                .replace("%keys%", Keys.values().size.toString())
+                .replace("%rewards%", Rewards.values().size.toString())
         )
     }
 }
