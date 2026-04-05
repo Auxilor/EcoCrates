41ec651f2279037fe930444728eb35659471b0c2
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-04-05 14:37:57 +0100

    Remove legacy reward file and crate keygui compatibility hooks

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecocrates/reward/Rewards.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecocrates/reward/Rewards.kt
@@ -2,15 +2,10 @@ package com.willfp.ecocrates.reward
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.libreforge.loader.LibreforgePlugin
-import com.willfp.libreforge.loader.configs.LegacyLocation
 import com.willfp.libreforge.loader.configs.RegistrableCategory
 
 object Rewards : RegistrableCategory<Reward>("reward", "rewards") {
 
-    override val legacyLocation = LegacyLocation(
-        "rewards.yml",
-        "rewards"
-    )
 
     override fun clear(plugin: LibreforgePlugin) {
         registry.clear()
